c54f33a7fde6225f15d5524ed7fb06f6cfa27e62
Martin Szarski <218236+mszarski@users.noreply.github.com>
2021-01-22 13:27:56 +1100

    Update simulation.cpp

--- arcsim/src/simulation.cpp
+++ arcsim/src/simulation.cpp
@@ -118,6 +118,8 @@ vector<Constraint*> get_constraints (Simulation &sim, bool include_proximity);
 void delete_constraints (const vector<Constraint*> &cons);
 void update_obstacles (Simulation &sim, bool update_positions=true);
 
+void update_constraints (Simulation &sim);
+
 void advance_step (Simulation &sim);
 
 void advance_frame (Simulation &sim) {
@@ -132,6 +134,7 @@ void advance_step (Simulation &sim) {
     sim.step++;
     // cout << "\t\tstep=" << sim.step << endl;
     update_obstacles(sim, false);
+    update_constraints(sim);
     vector<Constraint*> cons = get_constraints(sim, true);
     physics_step(sim, cons);
     plasticity_step(sim);
@@ -199,6 +202,14 @@ void physics_step (Simulation &sim, const vector<Constraint*> &cons) {
 void step_mesh (Mesh &mesh, Tensor dt) {
     for (int n = 0; n < mesh.nodes.size(); n++)
         mesh.nodes[n]->x = mesh.nodes[n]->x + mesh.nodes[n]->v*dt;
+}
+
+void update_constraints (Simulation &sim) {
+    for (int h = 0; h < sim.handles.size(); h++) {
+        Node *node = ((NodeHandle*)sim.handles[h])->get_nodes()[0];
+        node->x = node->x + node->v*sim.step_time;
+        node->xold = node->x;
+    }
 }
 
 void plasticity_step (Simulation &sim) {
